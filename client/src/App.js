748bb31f68fdd603e871cded5fb0a103c427a1df
leonitamataj <leonita.mataj19@gmail.com>
2023-04-12 17:21:57 +0200

    a
    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import Navbar from "./Navbar"
+import Home from "./pages/Home"
+import Departments from "./pages/Departments"
+import Patient from "./pages/Patient"
+import Doctors from "./pages/Doctors"
+import Services from "./pages/Services"
+import Branches from "./pages/Branches"
+import Login from "./pages/Login"
 import './App.css';
+import React, { useState } from "react";
+
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/departments" element={<Departments/>} />
+           <Route path="/services" element={<Services/>} />
+          <Route path="/doctors" element={<Doctors/>} />
+          <Route path="/branches" element={<Branches/>}/>
+          <Route path="/patient" element={<Patient/>} />
+          <Route path="/login" element={<Login/>} />
+          
+        </Routes>
+      </div>
+    </>
+    
+  )
 }
+
 
 export default App;
