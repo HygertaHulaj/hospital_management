298dcc16a43f85a2887d4d3087f2d6e035a61905
LeonitaMurati <94010514+LeonitaMurati@users.noreply.github.com>
2023-05-16 19:55:56 +0200

    homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,17 @@
-export default function Home() {
-    return <h1>Home</h1>
-  }
+import React from 'react'
+import GetInTouch from '../components/GetInTouch';
+import GetStarted from '../components/GetStarted';
+import Header from '../components/Header';
 
+const Home = () => {
+  return (
+    <>
+    <Header />
+    <GetStarted />
+    <GetInTouch />
+    </>
+
+  )
+}
+
+export default Home
