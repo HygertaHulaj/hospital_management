3a8846808a7cb1c1a0c6555b9b5118a2ccbe8c2e
HygertaHulaj <94575647+HygertaHulaj@users.noreply.github.com>
2023-05-21 22:27:53 +0200

    Create server

---
+++ server/main.py
@@ -0,0 +1,34 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
+
+@app.get("/")
+def hello():
+    return "Hello"
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
