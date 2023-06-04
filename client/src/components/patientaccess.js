6af5412b8f956c05b49249a2fe95760bc022cc8e
leonitamataj <leonita.mataj19@gmail.com>
2023-06-04 23:53:19 +0200

    uptade patient

--- client/src/components/patientaccess.js
+++ client/src/components/patientaccess.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Card, CardContent, CardMedia, Typography, Button, Link } from "@mui/material";
-import BloodDonation from './bloodDonation';
+import { Card, CardContent, CardMedia, Typography, Button } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 
 const PatientAccess = ({ title, description, imageUrl }) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -18,33 +18,55 @@ const PatientAccess = ({ title, description, imageUrl }) => {
     transition: "transform 0.2s ease-in-out",
   };
 
+  const handleTitleClick = () => {
+    // Redirect to another page
+    window.location.href = "./bloodDonation";
+  };
+
   return (
-    <Card
-      sx={{ width: 250, ...cardStyle }}
-      className="m-4"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <CardMedia component="img" height="200" image={imageUrl} alt={title} />
-      <CardContent>
-        <Typography variant="h6" component="h2" gutterBottom>
-          {title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {description}
-        </Typography>
-        <Button
-          component={Link}
-          to='./BloodDonation'
-          variant="contained"
-          color="primary"
-          size="small"
-          style={{ marginTop: 10, marginBottom: 15 }}
-        >
-          Read More
-        </Button>
-      </CardContent>
-    </Card>
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <Card
+        sx={{ width: 600, ...cardStyle }}
+        className="m-4"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <CardMedia component="img" height="200" image={imageUrl} alt={title} />
+        <CardContent>
+          <Typography variant="h6" component="h2" gutterBottom>
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+          <Button
+            component={RouterLink} // Use RouterLink instead of Link
+            to="/bloodDonation" // Specify the correct route path
+            variant="contained"
+            color="primary"
+            size="small"
+            style={{ marginTop: 10, marginBottom: 15 }}
+          >
+            Read More
+          </Button>
+        
+        </CardContent>
+      </Card>
+      <h1
+        style={{
+          backgroundColor: "transparent",
+          color: "black",
+          padding: "100px",
+          cursor: "pointer",
+          whiteSpace: "pre-wrap",
+          fontWeight: "bold",
+          fontSize: "30px",
+        }}
+        onClick={handleTitleClick}
+      >
+        Blood Donation
+      </h1>
+    </div>
   );
 };
 
--- client/src/pages/Patient.js
+++ client/src/pages/Patient.js
@@ -1,14 +1,13 @@
 import Topbar from '../components/topbar';
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import Patientaccess from "../components/patientaccess";
-import { Typography, Grid, Tab, Tabs} from "@mui/material";
-import { Box, margin } from '@mui/system';
+import { Typography, Grid, Tab, Tabs } from "@mui/material";
+import { Box } from '@mui/system';
 
 const Patient = () => {
   const [value, setValue] = useState(0);
 
-  
-  const patient = [
+  const patients = [
     {
       id: 1,
       title: "bloddonation",
@@ -17,33 +16,9 @@ const Patient = () => {
     },
     {
       id: 2,
-      title: "Service 2",
+      title: "blooddonationrequest",
       description: "Description of Service 2",
       imageUrl: "/service2.jpg",
-    },
-    {
-      id: 3,
-      title: "Service 3",
-      description: "Description of Service 3",
-      imageUrl: "/service3.jpg",
-    },
-    {
-      id: 4,
-      title: "Service 4",
-      description: "Description of Service 4",
-      imageUrl: "/service4.jpg",
-    },
-    {
-      id: 5,
-      title: "Service 5",
-      description: "Description of Service 5",
-      imageUrl: "/service5.jpg",
-    },
-    {
-      id: 6,
-      title: "Service 6",
-      description: "Description of Service 6",
-      imageUrl: "/service6.jpg",
     },
     // Add more services as needed
   ];
@@ -55,29 +30,29 @@ const Patient = () => {
   return (
     <div>
       <Topbar name="Our Patient" ></Topbar>
-      <Box style={{ backgroundColor: "#f0f0f0",margin:"26px"}}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="scrollable"
-        scrollButtons="auto"
-        aria-label="scrollable auto tabs example"
-      >
-      {patient.map((patient) => (
+      <Box style={{ backgroundColor: "#f0f0f0", margin: "26px" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
           <Grid container spacing={2}>
-            <Grid item key={patient.id} p={6} sx={{ marginTop: "50px" }} >
-              <Patientaccess
-                title={patient.title}
-                description={patient.description}
-                imageUrl={patient.imageUrl}
-              />
-            </Grid>
+            {patients.map((patient) => (
+              <Grid item key={patient.id} p={6} sx={{ marginTop: "50px" }} >
+                <Patientaccess
+                  title={patient.title}
+                  description={patient.description}
+                  imageUrl={patient.imageUrl}
+                />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Tabs>
-
+        </Tabs>
       </Box>
-      </div>
+    </div>
   );
 }
-export default Patient; 
+
+export default Patient;
